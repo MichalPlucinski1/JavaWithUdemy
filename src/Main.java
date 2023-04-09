ba52c61628a723b1883ae67c37caaf576e473a9d
Pluto <mic.plu@wp.pl>
2023-04-09 17:24:22 +0200

    update check

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(1000);
 
-        printYearsAndDays(561600);
+    //    printYearsAndDays(561600);
 
     }
 
