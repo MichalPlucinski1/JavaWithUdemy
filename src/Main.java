d8b56efb07c0414f099f16718e2ccf9ff869fdde
Pluto <mic.plu@wp.pl>
2023-04-09 17:23:29 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1000);
+
+        printYearsAndDays(561600);
+
+    }
+
+    public static boolean isCatPlaying   (boolean summer, int temperature){
+        if( (!summer) && ((temperature >= 25) && (temperature <= 35)) ){
+            return true;
+        }
+        if((summer) && ((temperature >= 25) && (temperature <= 45)) ){
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
