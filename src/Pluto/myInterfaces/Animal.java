e045c37496476f4d7ce93892eb2889b4fb55faa3
Pluto <mic.plu@wp.pl>
2023-07-17 13:23:18 +0200

    update on interfaces

--- src/Pluto/myInterfaces/Animal.java
+++ src/Pluto/myInterfaces/Animal.java
@@ -1,5 +1,61 @@
 package Pluto.myInterfaces;
 
+enum FlightStages implements Trackable{GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
+
+    @Override
+    public void track() {
+        if(this != GROUNDED){
+            System.out.println("Monitoring " + this);
+        }
+    }
+};
+
+
+record DragonFly(String name, String type) implements FlightEnabled{
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+
+class Satellite implements OrbitEarth{
+
+    @Override
+    public void achieveOrbit() {
+
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnabled{
+    void achieveOrbit();
+}
 interface FlightEnabled{
     void takeOff();
 
--- src/Pluto/myInterfaces/Main.java
+++ src/Pluto/myInterfaces/Main.java
@@ -1,5 +1,8 @@
 package Pluto.myInterfaces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Bird bird = new Bird();
@@ -17,14 +20,19 @@ public class Main {
 
         inFlight(flier);
 
+        ArrayList<FlightEnabled> fliers = new ArrayList<>();
+        fliers.add(bird);
+
+        List<FlightEnabled> betterFliers = new ArrayList<>();
+        betterFliers.add(bird);
     }
 
-    private static void inFlight(FlightEnabled flier){
+private static void inFlight(FlightEnabled flier){
         flier.takeOff();
         flier.fly();
         if(flier instanceof Trackable tracked){
-            tracked.track();
+        tracked.track();
         }
         flier.land();
-    }
+        }
 }
