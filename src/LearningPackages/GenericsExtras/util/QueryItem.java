29b01b9e3a624cf92cea35b6a2571ad447f04821
Pluto <mic.plu@wp.pl>
2023-07-20 14:51:26 +0200

    generics extras

---
+++ src/LearningPackages/GenericsExtras/util/QueryItem.java
@@ -0,0 +1,6 @@
+package LearningPackages.GenericsExtras.util;
+
+public interface QueryItem {
+    boolean matchFieldValue(String fieldName, String value);
+
+}
---
+++ src/LearningPackages/GenericsExtras/util/QueryList.java
@@ -0,0 +1,39 @@
+package LearningPackages.GenericsExtras.util;
+
+
+import LearningPackages.GenericsExtras.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryList <T extends Student & QueryItem> {
+
+    private List<T> items;
+
+    public QueryList(List<T> items) {
+        this.items = items;
+    }
+
+    public static <S extends QueryItem> List<S> getMatches(List<S> items,
+                                                           String field, String value) {
+
+        List<S> matches = new ArrayList<>();
+        for (var item : items) {
+            if (item.matchFieldValue(field, value)) {
+                matches.add(item);
+            }
+        }
+        return matches;
+    }
+
+    public List<T> getMatches(String field, String value) {
+
+        List<T> matches = new ArrayList<>();
+        for (var item : items) {
+            if (item.matchFieldValue(field, value)) {
+                matches.add(item);
+            }
+        }
+        return matches;
+    }
+}
