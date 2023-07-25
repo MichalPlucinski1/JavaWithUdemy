84e5fb124035852ae61c53cd20ef0fa8e4150057
Michal Plucinski <mic.plu@wp.pl>
2023-07-25 21:46:37 +0200

    Revert "some fast calculations programm"
    This reverts commit f7b619a88495bb27d806654f461e943213803935.

---
+++ src/Carpet.java
@@ -0,0 +1,11 @@
+public class Carpet {
+    private double cost;
+
+    public Carpet(double cost) {
+        this.cost = (cost<0)?0:cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
