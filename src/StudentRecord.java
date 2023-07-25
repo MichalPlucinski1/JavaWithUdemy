f7b619a88495bb27d806654f461e943213803935
Michal Plucinski <mic.plu@wp.pl>
2023-07-25 20:33:01 +0200

    some fast calculations programm

--- src/StudentRecord.java
+++
@@ -1,9 +0,0 @@
-public record StudentRecord(String name, int semester) {
-    @Override
-    public String toString() {
-        return "StudentRecord{" +
-                "name='" + name + '\'' +
-                ", semester=" + semester +
-                '}';
-    }
-}
